b5a9085554fda941509182953f945defb93fffd2
lizheng <2112276441@qq.com>
2021-02-05 18:12:50 +0800

    目录结构创建以及登录页面样式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入全局样式
+import './styles/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
